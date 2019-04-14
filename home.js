8af570dfe3c2929621e7eda462a0d36e9544d511
Aravindh036 <aravindh036@gmail.com>
2019-04-14 17:03:13 +0530

    single page navigation

--- home.js
+++ home.js
@@ -3,3 +3,32 @@ activate=()=>{
     console.log("jjj");
     document.getElementById("about-id").classList.toggle("active");
 }
+
+var home = `
+    <div class="user-location">
+        <div class="heading">Location</div>
+        <input type="text" class="location-id" placeholder="Enter location">
+    </div>
+`;
+
+var contact = `
+    <div>
+        Hello! contact me!!
+    </div>
+`;
+
+var gallery = `
+    <div>
+        photos
+    </div>
+`
+
+openGallery=()=>{
+    document.getElementById("container-id").innerHTML= gallery;
+}
+openHome=()=>{
+    document.getElementById("container-id").innerHTML= home;
+}
+openContact=()=>{
+    document.getElementById("container-id").innerHTML= contact;
+}
